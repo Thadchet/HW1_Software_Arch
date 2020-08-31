ab017f1f3eaf0e593a876e7552a2d74a8c446b4a
wutipong <wutipong@ava.fund>
2020-08-31 13:20:47 +0700

    first commit

---
+++ client.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const BASE_URL = 'http://localhost:3000';
+
+const listBooks = async () => {
+  const res = await axios.get(`${BASE_URL}/books`);
+
+  const books = res.data;
+
+  console.log(books);
+
+  return books;
+};
+
+const insertBook = async (id, title, author) => {
+  var book = { id: parseInt(id), title: title, author: author };
+
+  let res = await axios.post(`${BASE_URL}/book`,book);
+
+  console.log(res.data)
+}
+
+const getBook = async (id) => {
+  const res = await axios.get(`${BASE_URL}/book/${id}`);
+
+  const book = res.data;
+
+  console.log(book);
+
+  return book;
+};
+
+const deleteBook = async (id) => {
+  let res = await axios.delete(`${BASE_URL}/book/${id}`);
+
+  console.log(res.data)
+}
+
+var processName = process.argv.shift();
+var scriptName = process.argv.shift();
+var command = process.argv.shift();
+
+if (command == 'list')
+  listBooks();
+else if (command == 'insert')
+  insertBook(process.argv[0], process.argv[1], process.argv[2]);
+else if (command == 'get')
+  getBook(process.argv[0]);
+else if (command == 'delete')
+  deleteBook(process.argv[0]);
+else if (command == 'watch')
+  watchBooks();
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+
+const port = 3000;
+var books = [ 
+    { id: 123, title: 'A Tale of Two Cities', author: 'Charles Dickens' }
+];
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+    console.log(book);
+    books.push(book);
+    res.send('Book is added to the database');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+app.get('/book/:id',(req,res) => {
+    let target;
+    const id = parseInt(req.params.id);
+    for (var i = 0; i < books.length; i++){
+        if (books[i].id == id){
+            target = books[i];
+        }
+    }
+    res.json(target);
+});
+
+app.delete('/book/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].id == id) {
+            books.splice(i, 1);
+            // return true;
+        }
+    }
+    res.send('Book is deleted');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+
