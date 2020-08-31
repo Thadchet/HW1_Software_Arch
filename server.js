4c43599de141a33c65c64dff27bea0a00a50251c
Thadchet kittichotikul <42507576+Thadchet@users.noreply.github.com>
2020-08-31 16:20:34 +0700

    add swagger

--- server.js
+++ server.js
@@ -8,7 +8,53 @@ const port = 3000;
 var books = [
   { id: 123, title: "A Tale of Two Cities", author: "Charles Dickens" },
 ];
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
+const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "2.0.0",
+        title: "Book API",
+        description: "Book API Information",
+        contact: {
+          name: "Amazing Developer"
+        },
+        servers: ["http://localhost:3000"]
+      }
+    },
+    // ['.routes/*.js']
+    apis: ["server.js"]
+  };
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+/**
+ * @swagger
+ * /book:
+ *    post:
+ *      description: Use to return all customers
+ *    parameters:
+ *      - name: id
+ *        description: Id of the book
+ *        required: true
+ *        schema:
+ *          type: integer
+ *      - name: title
+ *        description: title of the book
+ *        required: true
+ *        schema:
+ *          type: string
+ *      - name: author
+ *        description: author of the book
+ *        required: true
+ *        schema:
+ *          type: string    
+ *    responses:
+ *      '201':
+ *        description: Successfully created user
+ */
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,9 +71,34 @@ app.post("/book", (req, res) => {
   res.send("Book is added to the database");
 });
 
+/**
+ * @swagger
+ * /books:
+ *  get:
+ *    description: Use to get all the books
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 app.get("/books", (req, res) => {
   res.json(books);
 });
+
+/**
+ * @swagger
+ * /book:
+ *  get:
+ *    description: Use to get the books
+ *    parameters:
+ *      - name: id
+ *        description: Id of the book
+ *        required: true
+ *        schema:
+ *          type: integer
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 
 app.get("/book/:id", (req, res) => {
   let target;
@@ -51,6 +122,15 @@ app.delete("/book/:id", (req, res) => {
   }
   res.send("Book is deleted");
 });
+/**
+ * @swagger
+ * /books:
+ *  delete:
+ *    description: Use to delete the book bt id
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 
 server = app.listen(port, () => {
   console.log(`Hello world app listening on port ${port}!`);
